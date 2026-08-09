8f4acaca65e8bbf83bc6dd798d5dbd40613a2fc9
Kirbinator-rgb <dc84q45@gmail.com>
2026-08-09 06:10:33 -0400

    fix: disable arbitrary SQL lesson queries

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson2.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson2.java
@@ -4,15 +4,8 @@
  */
 package org.owasp.webgoat.lessons.sqlinjection.introduction;
 
-import static java.sql.ResultSet.CONCUR_READ_ONLY;
-import static java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE;
 import static org.owasp.webgoat.container.assignments.AttackResultBuilder.failed;
-import static org.owasp.webgoat.container.assignments.AttackResultBuilder.success;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import org.owasp.webgoat.container.LessonDataSource;
 import org.owasp.webgoat.container.assignments.AssignmentEndpoint;
 import org.owasp.webgoat.container.assignments.AssignmentHints;
 import org.owasp.webgoat.container.assignments.AttackResult;
@@ -30,13 +23,6 @@ import org.springframework.web.bind.annotation.RestController;
       "SqlStringInjectionHint2-4"
     })
 public class SqlInjectionLesson2 implements AssignmentEndpoint {
-
-  private final LessonDataSource dataSource;
-
-  public SqlInjectionLesson2(LessonDataSource dataSource) {
-    this.dataSource = dataSource;
-  }
-
   @PostMapping("/SqlInjection/attack2")
   @ResponseBody
   public AttackResult completed(@RequestParam String query) {
@@ -44,22 +30,6 @@ public class SqlInjectionLesson2 implements AssignmentEndpoint {
   }
 
   protected AttackResult injectableQuery(String query) {
-    try (var connection = dataSource.getConnection()) {
-      Statement statement = connection.createStatement(TYPE_SCROLL_INSENSITIVE, CONCUR_READ_ONLY);
-      ResultSet results = statement.executeQuery(query);
-      StringBuilder output = new StringBuilder();
-
-      results.first();
-
-      if (results.getString("department").equals("Marketing")) {
-        output.append("<span class='feedback-positive'>" + query + "</span>");
-        output.append(SqlInjectionLesson8.generateTable(results));
-        return success(this).feedback("sql-injection.2.success").output(output.toString()).build();
-      } else {
-        return failed(this).feedback("sql-injection.2.failed").output(output.toString()).build();
-      }
-    } catch (SQLException sqle) {
-      return failed(this).feedback("sql-injection.2.failed").output(sqle.getMessage()).build();
-    }
+    return failed(this).feedback("sql-injection.2.failed").build();
   }
 }
--- src/test/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson2Test.java
+++ src/test/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson2Test.java
@@ -15,12 +15,13 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 public class SqlInjectionLesson2Test extends LessonTest {
 
   @Test
-  public void solution() throws Exception {
+  public void arbitrarySqlIsNotExecuted() throws Exception {
     mockMvc
         .perform(
             MockMvcRequestBuilders.post("/SqlInjection/attack2")
                 .param("query", "SELECT department FROM employees WHERE userid=96134;"))
         .andExpect(status().isOk())
-        .andExpect(jsonPath("$.lessonCompleted", CoreMatchers.is(true)));
+        .andExpect(jsonPath("$.lessonCompleted", CoreMatchers.is(false)))
+        .andExpect(jsonPath("$.output", CoreMatchers.nullValue()));
   }
 }
